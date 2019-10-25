b0ea51bb7373a8fb8e9ed2ed6d5a42c16a311940
wasan <Sscs@123>
2019-10-25 14:03:33 +0700

    test

---
+++ src/main/java/com/iphayao/linebot/WebController.java
@@ -0,0 +1,16 @@
+package com.iphayao.linebot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class WebController {
+
+    @GetMapping("/*")
+    public String processCompareImage(){
+
+        return "Hello world";
+    }
+}
